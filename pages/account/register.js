2ef4290675932ea63932f34b5ba3d2fed046f0f9
Jorgen Arnes <jorgen.arnes@gmail.com>
2021-08-12 01:41:41 +0200

    Switch to getServerSideProps in slug page

---
+++ pages/account/register.js
@@ -0,0 +1,69 @@
+
+import { FaUser } from 'react-icons/fa'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { useState, useEffect, useContext } from 'react'
+import Link from 'next/link'
+import Layout from '@/components/Layout'
+import AuthContext from '@/context/AuthContext'
+import styles from '@/styles/AuthForm.module.css'
+
+
+export default function RegisterPage() {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordConfirm, setPasswordConfirm] = useState('')
+
+    const {register, error} = useContext(AuthContext)
+
+    useEffect(() => {
+        error && toast.error(error)
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if(password !== passwordConfirm) {
+            toast.error('Password do not match!')
+            return
+        }
+        register({username, email, password})
+    }
+
+    return (
+        <Layout title='User Register'>
+          <div className={styles.auth}>
+              <h1>
+                  <FaUser /> Register
+              </h1>
+              <ToastContainer />
+              <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" value={username} id="username" onChange={(e) => setUsername(e.target.value)} />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email Address</label>
+                        <input type="email" value={email} id="email" onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" value={password} id="password" onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <div>
+                        <label htmlFor="passwordConfirm">Confirm Password</label>
+                        <input type="password" value={passwordConfirm} id="passwordConfirm" onChange={(e) => setPasswordConfirm(e.target.value)} />
+                    </div>
+
+                    <input type="submit" value="Login" className='btn' />
+              </form>
+
+              <p>
+                  Already have an account? <Link href='/account/login'>Register</Link> 
+              </p>
+          </div>
+        </Layout>
+    )
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,37 @@
-import Layout from '../components/Layout'
+import {useEffect} from 'react'
+import Link from 'next/link'
+import Layout from '@/components/Layout'
+import Eventitem from '@/components/Eventitem'
+import { API_URL } from '@/config/index'
 
-export default function Home() {
+export default function HomePage({events}) {
+  useEffect(() => {
+ 
+  }, [events])
   return (
     <Layout>
-     <h1>Home</h1>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <Eventitem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href='/events'>
+            <a className='btn-secondary'>View All Events</a>
+        </Link>  
+      )}
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=3`)
+  const events = await res.json()
+ 
+  return {
+    props: {events},
+    revalidate: 1
+  }
 }
